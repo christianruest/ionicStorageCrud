1d4231f2d37ec588c00ca18f27b76507b08e203a
christianruest <christian.ruest@hotmail.com>
2019-12-19 16:39:38 +0100

    created CRUD service and simple UI to save and read from storage

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 following a tutorial to create simple crud operations on ionic storage
 
+storage is configured to write using indexeddb (app.module.ts)
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { StorageService, Item } from '../services/storage.service';
+import { Platform, ToastController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,60 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  items: Item[] = [];
+
+  newItem: Item = <Item>{};
+
+  @ViewChild('mylist', {static: false}) mylist: IonList;
+
+  constructor(private storageService: StorageService, private plt: Platform, private toastController: ToastController) {
+    this.plt.ready().then(() => {
+      this.loadItems();
+    });
+  }
+
+  addItem() {
+    this.newItem.modified = Date.now();
+    this.newItem.id = Date.now();
+
+    this.storageService.addItem(this.newItem).then(item => {
+      this.newItem = <Item>{};
+      this.showToast( + ' added!');
+      this.loadItems();
+    })
+  }
+
+  loadItems() {
+    this.storageService.getItems().then(items => {
+      this.items = items;
+    });
+  }
+
+  updateItem(item: Item) {
+    item.title = `UPDATED: ${item.title}`;
+    item.modified = Date.now();
+
+    this.storageService.updateItem(item).then(item => {
+      this.showToast('Item updated');
+      this.mylist.closeSlidingItems();
+      this.loadItems();
+    });
+  }
+
+  deleteItem(item: Item) {
+    this.storageService.deleteItem(item.id).then(item => {
+      this.showToast('Item removed!');
+      this.mylist.closeSlidingItems();
+      this.loadItems();
+    })
+  }
+
+  async showToast(msg) {
+    const toast = await this.toastController.create({
+      message:msg,
+      duration: 2000
+    });
+    toast.present();
+  }
 
 }
